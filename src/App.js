ca90820be5657b698fead80cf0f0df9a83e2a158
bpeterson1663 <bpeterson1663@gmail.com>
2018-01-06 22:02:35 -0600

    Layout Container Applied

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
       <div>
-       
+        <Layout>
+          <p>Hello</p>
+        </Layout>
       </div>
     );
   }
