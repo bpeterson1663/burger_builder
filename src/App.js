a85bb8baf1d8d65be5f0b8f0363557ae35d0d361
bpeterson1663 <bpeterson1663@gmail.com>
2018-02-20 21:49:24 -0600

    Orders Page Functioning

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Route} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,8 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+
         </Layout>
       </div>
     );
