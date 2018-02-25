d14ed2266c6d70ede541405a27dadb177bd484c6
bpeterson1663 <bpeterson1663@gmail.com>
2018-02-24 19:06:24 -0600

    Form Validation working

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ import classes from './CheckoutSummary.css';
 const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
-            <h1>We hope it tastes well1</h1>
+            <h1>We hope it tastes well</h1>
             <div style={{width: '100%', height: '300px', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
